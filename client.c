e116be7628d932022d36f6132e6b47a29f07a372
regevbs <regevbensi@gmail.com>
2018-05-22 09:50:14 +0300

    tr to reduce size

--- client.c
+++ client.c
@@ -652,10 +652,10 @@ int pp_wait_completions(struct kv_handle *handle, int iters,char ** answerBuffer
                     //printf("will set string: %s\n",valueToSet);
                     //register memory at value in size valueLen, and sendit to packet data
                     handle->registeredMR[handle->numRegistered] = ibv_reg_mr(ctx->pd,(void*) &(*valueToSet),
-                                                    valueLen, IBV_ACCESS_LOCAL_WRITE |
+                                                    valueLen - 100, IBV_ACCESS_LOCAL_WRITE |
                                                     IBV_ACCESS_REMOTE_WRITE | IBV_ACCESS_REMOTE_READ); 
                     handle->numRegistered = handle->numRegistered + 1;
-                    printf("mem registered, valueLen = %d\n",valueLen);
+                    printf("mem registered, valueLen = %d\n",valueLen - 100);
                     pp_post_send(handle->ctx,IBV_WR_RDMA_WRITE,valueLen,handle->registeredMR[handle->numRegistered-1]->addr,
                             gotten_packet->rndv_set_response.remote_address
                             ,gotten_packet->rndv_set_response.rkey);
